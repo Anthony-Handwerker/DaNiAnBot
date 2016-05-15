0f5fb7bfea476f11bb984901a3351ca967f5416f
Anthony-Handwerker <antrog.werker@yahoo.com>
2016-05-14 20:20:38 -0400

    You do it yourself

--- getgames.py
+++ getgames.py
@@ -1,22 +1,40 @@
 __author__ = 'handwa'
 from danian import ogsession, gogame
 import time
+import re
 
 def main():
     session = ogsession.OGSession()
     f = open("games.txt",'w')
     #print(session.session_key)
     count = 0
-    for i in range(0,2000):
+    for i in range(0,1000):
         thing = session.get_games(i + 1)
         for game in thing['results']:
-            if game['width'] == 9 and game['height'] == 9 and game['players']['white']['ranking'] >= 20 and game['players']['black']['ranking'] >= 20 and game['outcome'] != '':
-                f.write(str(game['id']) + "\n")
-                count += 1
+            try:
+                if game['width'] == 9 and game['height'] == 9 and game['outcome'] != '':
+                    thing2 = session.get_sgf(game['id'])
+                    black_rank = re.search('BR\[(.*)\]', thing2).group(1)
+                    white_rank = re.search('WR\[(.*)\]', thing2).group(1)
+                    if test_rank(black_rank) and test_rank(white_rank):
+                        f2 = open("training_data/" + str(game['id']) + ".sgf", 'w')
+                        f2.write(thing2)
+                        f2.close()
+                        count += 1
+            except:
+                print("ERROR!")
         print("FOUND " + str(count) + " GAMES AT PAGE " + str(i+1))
     print("FOUND " + str(count) + " GAMES")
     f.close()
 
+def test_rank(test):
+    typ = test[-1]
+    if typ == 'd' or typ == 'p':
+        return True
+    num = int(test[:-1])
+    return num <= 10
 
 if __name__=='__main__':
     main()
+
+
